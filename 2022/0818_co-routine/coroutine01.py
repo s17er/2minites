4f453e6243953bfa03a227f6953903d4c03de49e
s17er <sinnerthesinner@gmail.com>
2022-08-19 22:20:43 +0900

    decorator function

--- 2022/0818_co-routine/coroutine01.py
+++ 2022/0818_co-routine/coroutine01.py
@@ -1,3 +1,13 @@
+from functools import wraps
+
+def coroutine(f):
+    @wraps(f)
+    def primer(*args, **kwargs):
+        g = f(*args, **kwargs)
+        next(g)
+        return g
+    return primer
+
 """
     y = yes()
     print(next(y))
@@ -19,9 +29,48 @@ def recv():
         v = yield
         print(f'received: {v}')
         
-if __name__ == '__main__':
-    receiver = recv()
-    next(receiver)
+"""
+    receiver = recv2()
     receiver.send("1")
     receiver.send("2")
     receiver.send("3")
+"""
+@coroutine
+def recv2():
+    print('start.')
+    while True:
+        v = yield
+        print(f'received: {v}')
+        
+        
+"""
+    c = counter()
+    c.send(1)
+    c.send(2)
+    c.send(3)
+    try:
+        c.send(None)
+    except StopIteration as e:
+        res = e.value
+    res
+"""
+@coroutine
+def counter():
+    count = 0
+    while True:
+        by = yield
+        if by is None:
+            break
+        count += by
+    return count
+
+if __name__ == '__main__':
+    c = counter()
+    c.send(1)
+    c.send(2)
+    c.send(3)
+    try:
+        c.send(None)
+    except StopIteration as e:
+        res = e.value
+    print(res)
