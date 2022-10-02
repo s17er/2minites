6ff1a5b5ca59366d76527e3a525a22876a1a6db4
s17er <sinnerthesinner@gmail.com>
2022-10-02 18:15:36 +0900

    lru cache

---
+++ 2022/fluent_python/cp07_lrucache.py
@@ -0,0 +1,12 @@
+import functools
+from libs import clock
+
+@functools.lru_cache()
+@clock.clock
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-2) + fibonacci(n-1)
+
+if __name__ == '__main__':
+    print(fibonacci(6))
