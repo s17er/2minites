f5c8071d26bbb817e86d63c6d63a1b831e07400a
s17er <sinnerthesinner@gmail.com>
2022-11-09 18:41:45 +0900

    format

--- 2022/fluent_python/cp10_vector.py
+++ 2022/fluent_python/cp10_vector.py
@@ -5,6 +5,7 @@ import math
 import numbers
 import functools
 import operator
+import itertools
 
 class Vector:
     
@@ -87,6 +88,28 @@ class Vector:
         msg = '{.__name__!r} object has not attribute {!r}' # !r is repr()
         raise AttributeError(msg.format(cls, name))
     
+    def angle(self, n):
+        r = math.sqrt(sum(x * x for x in self[n:]))
+        a = math.atan2(r, self[n-1])
+        if (n == len(self) - 1) and (self[-1] < 0):
+            return math.pi * 2 - a
+        else:
+            return a
+        
+    def angles(self):
+        return (self.angle(n) for n in range(1, len(self)))
+    
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('h'):
+            fmt_spec = fmt_spec[:-1]
+            coords = itertools.chain([abs(self)], self.angles())
+            outer_fmt = '<{}>'
+        else:
+            coords = self
+            outer_fmt = '({})'
+        componets = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(', '.join(componets))
+
     @classmethod
     def frombytes(cls, octets):
         typecode = chr(octets[0])
@@ -121,4 +144,8 @@ if __name__ == '__main__':
     v2 = Vector([3.0, 4.0, 5.0, 6,0, 7.0, 8.0, 10.0])
     print(v1==v2)
     print(v==v2)
+
+    print(format(v1, 'h'))
+    print(format(v1, '.3eh'))
+    print(format(v1, '.3e'))
     
