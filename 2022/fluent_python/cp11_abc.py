57809594b20c5b1b07e54e78dbfa65471d4b364b
s17er <sinnerthesinner@gmail.com>
2022-11-14 18:03:12 +0900

    subclass

--- 2022/fluent_python/cp11_abc.py
+++ 2022/fluent_python/cp11_abc.py
@@ -1,3 +1,4 @@
+import random
 import abc
 
 class Tombola(abc.ABC):
@@ -13,10 +14,47 @@ class Tombola(abc.ABC):
         This method should raise 'LookupError' when the instance is empty.
         """
         
-    def load(self):
+    def loaded(self):
         """Return `True` if there's at least 1 item, `False` otherwise."""
         return bool(self.inspect())
     
     def inspect(self):
         """Return a sorted tuple with the items currently inside."""
+        items = []
+        while True:
+            try:
+                items.append(self.pick())
+            except LookupError:
+                break
+        self.load(items)
+        return tuple(sorted(items))
+    
+class BingoCage(Tombola):
+    
+    def __init__(self, items) -> None:
+        self._randomizer = random.SystemRandom()
+        self._items = []
+        self.load(items)
+        
+    def load(self, items):
+        self._items.extend(items)
+        self._randomizer.shuffle(self._items)
+        
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('pick from empty BingoCage')
+        
+    def __call__(self):
+        self.pick()
+
+
+if __name__ == '__main__':
+    cage = BingoCage(range(100))
+    while True:
+        try:
+            print(cage.pick())
+        except LookupError:
+            break
 
