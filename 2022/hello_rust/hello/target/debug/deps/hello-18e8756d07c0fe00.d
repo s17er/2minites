ef7eb521420266e87ad2ce48771de7e735d05959
s17er <sinnerthesinner@gmail.com>
2022-11-17 16:48:55 +0900

    rust gitignore

--- 2022/hello_rust/hello/target/debug/deps/hello-18e8756d07c0fe00.d
+++
@@ -1,5 +0,0 @@
-/Users/s17er/Repos/s17er/2mintes/2022/hello_rust/hello/target/debug/deps/hello-18e8756d07c0fe00: src/main.rs
-
-/Users/s17er/Repos/s17er/2mintes/2022/hello_rust/hello/target/debug/deps/hello-18e8756d07c0fe00.d: src/main.rs
-
-src/main.rs:
--- 2022/hello_rust/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/Users/s17er/Repos/s17er/2mintes/2022/hello_rust/hello/target/debug/hello: /Users/s17er/Repos/s17er/2mintes/2022/hello_rust/hello/src/main.rs
