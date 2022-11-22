a9a30719d6f8e091676d0b97a93dbd5349c98b4b
s17er <sinnerthesinner@gmail.com>
2022-11-22 17:33:52 +0900

    rust borrow check

--- 2022/hello_rust/hello/src/main.rs
+++ 2022/hello_rust/hello/src/main.rs
@@ -2,5 +2,19 @@ fn main() {
     for n in 0..10 {
       println!("{n}: Hello, Rust!");
     }
-    //println!("Hello, world!");
+
+    let a = 10;
+    let aref1 = &a;
+    let aref2 = &a;
+    println!("{}, {}, {}", a, aref1, aref2);
+
+    let mut a = 10;
+    let a_ref1 = &a;
+    let a_mut_ref1 = &mut a;
+    let a_mut_ref2 = &mut a;
+    //*a_mut_ref1 = 20; // error.
+    *a_mut_ref2 = 20;
+    println!("{}", a);
+    //println!("{}", a_ref1); // error.
+
 }
