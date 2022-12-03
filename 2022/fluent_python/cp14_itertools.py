09fc4168a4c8d348f7cb05a4e2c5f09200826a10
s17er <sinnerthesinner@gmail.com>
2022-12-03 15:56:46 +0900

    itertools

---
+++ 2022/fluent_python/cp14_itertools.py
@@ -0,0 +1,14 @@
+import itertools
+
+def vowel(c):
+    return c.lower() in 'aeiou'
+
+if __name__ == '__main__':
+    print(list(filter(vowel, 'Aardvark')))
+    print(list(itertools.filterfalse(vowel, 'Aardvark')))
+    print(list(itertools.dropwhile(vowel, 'Aardvark')))
+    print(list(itertools.takewhile(vowel, 'Aardvark')))
+    print(list(itertools.compress('Aardvark', (1,0,1,1,0,1))))
+    print(list(itertools.islice('Aardvark', 4)))
+    print(list(itertools.islice('Aardvark', 4, 7)))
+    print(list(itertools.islice('Aardvark', 1, 7, 2)))
