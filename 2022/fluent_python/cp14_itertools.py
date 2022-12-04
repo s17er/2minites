23c306ee6f1c57d6f2d230d6169bf7254193d4a5
s17er <sinnerthesinner@gmail.com>
2022-12-04 17:40:08 +0900

    itertools map

--- 2022/fluent_python/cp14_itertools.py
+++ 2022/fluent_python/cp14_itertools.py
@@ -1,9 +1,11 @@
 import itertools
+import operator
 
 def vowel(c):
     return c.lower() in 'aeiou'
 
 if __name__ == '__main__':
+    # filter
     print(list(filter(vowel, 'Aardvark')))
     print(list(itertools.filterfalse(vowel, 'Aardvark')))
     print(list(itertools.dropwhile(vowel, 'Aardvark')))
@@ -12,3 +14,16 @@ if __name__ == '__main__':
     print(list(itertools.islice('Aardvark', 4)))
     print(list(itertools.islice('Aardvark', 4, 7)))
     print(list(itertools.islice('Aardvark', 1, 7, 2)))
+    
+    # map
+    sample = [5, 4, 2, 8, 7, 6, 3, 0, 9, 1]
+    print(list(itertools.accumulate(sample)))
+    print(list(itertools.accumulate(sample, min)))
+    print(list(itertools.accumulate(sample, max)))
+    print(list(itertools.accumulate(sample, operator.mul)))
+    print(list(itertools.accumulate(range(1, 11), operator.mul)))
+    print(list(enumerate('albatroz', 1)))
+    print(list(map(operator.mul, [0, 1, 2], [3, 4, 5]))) # 0*3, 1*4, 2*5
+    print(list(map(lambda a, b: (a, b), [0, 1, 2], [3, 4, 5]))) 
+    print(list(itertools.starmap(operator.mul, enumerate('albatroz', 1))))
+    print(list(itertools.starmap(lambda a, b: b/a, enumerate(itertools.accumulate(sample), 1))))
